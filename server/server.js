9bc2cca2b078afd6eeefb2c073e1a4be6cce0111
preston <mechampreston@gmail.com>
2017-02-26 00:05:59 -0700

    More Initial Setup

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const react = require('react');
+const ReactRouter = require('react-router');
+const port = 3000;
+const fs = require('fs');
+const baseTemplate = fs.readFileSync('../public/index.html');
+const template = _.template(baseTemplate);
+const App = require('../public/js/App').default;
+
+const server = express();
+
+server.use('/public', express.static('./public'))
+
+server.use((req, res) => {
+  const context = ReactRouter.createServerRenderContext()
+  let body = ReactDOMServer.renderToString(
+    React.createElement(ServerRouter, {location: req.url, context: context},
+      React.createElement(App)
+    )
+  )
+  res.write(template({body: body}))
+  res.end()
+})
+
+console.log('listening on ' + port)
+server.listen(port)
