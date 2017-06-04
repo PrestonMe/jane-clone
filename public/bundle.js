fd9d85421b9be68a0356b3e1491371eb507cc7ab
preston <mechampreston@gmail.com>
2017-06-04 01:03:47 -0600

    Deployment Bundle

--- public/bundle.js
+++ public/bundle.js
@@ -3424,31 +3424,6 @@ var SignIn = function (_React$Component) {
     return value === false ? 'invalid' : '';
   };
 
-  SignIn.prototype.componentDidMount = function componentDidMount() {
-    // window.fbAsyncInit = function() {
-    //   FB.init({
-    //     appId      : '432409103793644',
-    //     cookie     : true,
-    //     xfbml      : true,
-    //     version    : 'v2.8'
-    //   });
-    //   FB.AppEvents.logPageView()
-    //   FB.getLoginStatus(function(response) {
-    //     console.log(response)
-    //     // statusChangeCallback(response);
-    //   });
-    // };
-    //
-    // (function(d, s, id){
-    //    var js, fjs = d.getElementsByTagName(s)[0];
-    //    if (d.getElementById(id)) {return;}
-    //    js = d.createElement(s); js.id = id;
-    //    js.src = "//connect.facebook.net/en_US/sdk.js";
-    //    fjs.parentNode.insertBefore(js, fjs);
-    //  }(document, 'script', 'facebook-jssdk'));
-
-  };
-
   SignIn.prototype.render = function render() {
     return __WEBPACK_IMPORTED_MODULE_0_react__["default"].createElement(
       'div',
